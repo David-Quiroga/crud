b8b2549a53aa2f7cb41fb453500a360d35f71402
unknown <dqdavudquiroga@gmail.com>
2023-12-04 08:02:02 -0500

    Cambios en operation

--- lib/src/db/operation.dart
+++ lib/src/db/operation.dart
@@ -2,10 +2,11 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 class Operation {
-  Future<Database> _openDB(){
-    return openDatabase(
-      join(getDatabasesPath(), 
-      "notes.db")
-    );
+  Future<Database> _openDB() {
+    return openDatabase(join(getDatabasesPath() as String, "note.db"),
+        onCreate: (db, version) {
+      return db.execute(
+          "CREATE TABLE notes (id INTEGER PRIMARY KEY, title TEXT, content TEXT)");
+    }, version: 1);
   }
 }
